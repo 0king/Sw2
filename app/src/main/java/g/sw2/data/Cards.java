95e1f91cdc78f20fc631ec6894e5753f9bb93b3b
Kush Agrawal <kush.agrawal39@gmail.com>
2017-04-10 02:36:31 +0530

    Changed Card Screen

---
+++ app/src/main/java/g/sw2/data/Cards.java
@@ -0,0 +1,14 @@
+package g.sw2.data;
+
+import java.util.List;
+
+/**
+ * Created by Kush Agrawal on 4/4/2017.
+ */
+
+public class Cards {
+
+    private List<StudyCards> study_cards;
+    private String exercise_cards_related_topic;
+    private List<ExerciseLevel> exercise_cards_difficulty_levels;
+}
